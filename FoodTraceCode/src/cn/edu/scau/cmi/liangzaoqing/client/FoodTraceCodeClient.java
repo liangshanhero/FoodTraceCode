c9c10aebb143eeaa1182d6e392d534d70b23288a
xiao <xiao@xiao-PC>
2017-12-25 10:40:14 +0800

    人头

--- FoodTraceCode/src/cn/edu/scau/cmi/liangzaoqing/client/FoodTraceCodeClient.java
+++ FoodTraceCode/src/cn/edu/scau/cmi/liangzaoqing/client/FoodTraceCodeClient.java
@@ -2,10 +2,18 @@ package cn.edu.scau.cmi.liangzaoqing.client;
 
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Combo;
 
 public class FoodTraceCodeClient {
 
 	protected Shell shell;
+	private Text text;
+	private Text text_1;
+	private Text text_2;
+	private Text text_3;
 
 	/**
 	 * Launch the application.
@@ -40,9 +48,54 @@ public class FoodTraceCodeClient {
 	 */
 	protected void createContents() {
 		shell = new Shell();
-		shell.setSize(450, 300);
-		shell.setText("农产品追溯码生成器");
+		shell.setSize(628, 433);
+		shell.setText("农产品追溯二维码生成器");
+		
+		text = new Text(shell, SWT.BORDER);
+		text.setBounds(112, 21, 490, 23);
+		
+		Label label = new Label(shell, SWT.NONE);
+		label.setBounds(26, 24, 80, 17);
+		label.setText("农产品追溯码");
+		
+		Label lblNewLabel = new Label(shell, SWT.NONE);
+		lblNewLabel.setBounds(26, 70, 61, 17);
+		lblNewLabel.setText("追溯码加密");
+		
+		Label label_1 = new Label(shell, SWT.NONE);
+		label_1.setText("纠错编码加密");
+		label_1.setBounds(26, 115, 84, 17);
+		
+		Label label_2 = new Label(shell, SWT.NONE);
+		label_2.setText("图像加密");
+		label_2.setBounds(26, 157, 61, 17);
+		
+		Combo combo = new Combo(shell, SWT.NONE);
+		combo.setItems(new String[] {"不加密", "RSA", "DES", "MD5", "SHA-1"});
+		combo.setBounds(112, 67, 108, 25);
+		combo.setText("请选择加密方式");
+		
+		text_1 = new Text(shell, SWT.BORDER);
+		text_1.setText("请输入密码");
+		text_1.setBounds(226, 70, 376, 23);
+		
+		Combo combo_1 = new Combo(shell, SWT.NONE);
+		combo_1.setItems(new String[] {"不加密", "RSA", "DES", "MD5", "SHA-1"});
+		combo_1.setBounds(112, 107, 108, 25);
+		combo_1.setText("请选择加密方式");
+		
+		Combo combo_2 = new Combo(shell, SWT.NONE);
+		combo_2.setItems(new String[] {"不加密", "RSA", "DES", "MD5", "SHA-1"});
+		combo_2.setBounds(112, 149, 108, 25);
+		combo_2.setText("请选择加密方式");
+		
+		text_2 = new Text(shell, SWT.BORDER);
+		text_2.setText("请输入密码");
+		text_2.setBounds(226, 109, 376, 23);
+		
+		text_3 = new Text(shell, SWT.BORDER);
+		text_3.setText("请输入密码");
+		text_3.setBounds(226, 151, 376, 23);
 
 	}
-
 }
